1e8be0f43a49dae1a2789c40a82fc61b95cd3834
秋本 裕之 <hiroyuki.akimoto@willof.co.jp>
2021-10-05 15:15:57 +0900

    [fix]step_contact_form

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer id="footer" class="mt20">
+    <div class="container">
+        <p>2021 株式会社◯◯</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style scoped>
+#footer{
+  background: #35495e;
+}
+#footer p{
+  padding: 10px 0;
+  text-align: center;
+  color: #fff;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <header>
+    <div class="container">
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+header{
+  border-bottom: 2px solid #0e271c;
+}
+.container h1{
+  position: relative;
+  text-align: center;
+  height: 50px;
+  line-height: 50px;
+}
+.container h1 img{
+  display: inline;
+  text-align: center;
+  margin: auto;
+  height: 50px;
+}
+</style>
